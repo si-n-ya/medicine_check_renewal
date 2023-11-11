0bc5918e1cc6cff3a6b6841a9f615bdfd0e01022
github user si-n-ya <sin.982.9.s@gmail.com>
2023-11-12 00:50:06 +0900

    「服用時刻」の更新処理を修正
    - 更新時に不要な「服用時刻」が削除されず、新しく登録のみされていたため、不要になった時刻はDBから削除した上で、更新もしくは新規に登録

--- src/app/Services/Medicine/UpdateMedicineService.php
+++ src/app/Services/Medicine/UpdateMedicineService.php
@@ -12,7 +12,8 @@ class UpdateMedicineService
     /**
      * お薬の登録
      *
-     * @param \Illuminate\Http\Request
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Medicine $medicine
      * @throws \Exception
      */
     public function handle(Request $request, Medicine $medicine)
@@ -25,8 +26,19 @@ class UpdateMedicineService
           $medicine->update($request->all());
           
           $medicine->daysOfWeek()->sync($request->day_of_weeks);
+          
+          // 登録されている時刻データを取得
+          $currentTimes = $medicine->medicineTimes->pluck('time_of_day')->toArray();
+          // チェックから外された不要な時刻データを削除
+          foreach ($currentTimes as $currentTime) {
+            if (!in_array($currentTime, $request->times)) {
+                $medicine->medicineTimes()->where('time_of_day', $currentTime)->delete();
+            }
+          }
+
+          // 時刻を更新 or 新しく登録
           foreach ($request->times as $time) {
-              $medicine->medicineTimes()->create([
+              $medicine->medicineTimes()->updateOrCreate([
                   'time_of_day' => $time,
               ]);
           }
