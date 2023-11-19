23d9a149395fb185f08e4ec2e20a503ea6a0d1ae
github user si-n-ya <sin.982.9.s@gmail.com>
2023-11-19 20:38:13 +0900

    FullCalendarのインタラクションとナビゲーションを更新
    - prev、next、todayボタンクリック時の動的なURL更新を実装
    - prev、nextボタンクリック時は、移動した月の「1日」が選択されるようにする
    - 'today'ボタンクリック時にカレンダービューが現在の月に更新されるようにする
    - day.jsを使用した日付フォーマットで正確な月ナビゲーションを実装

--- src/resources/ts/pages/medicines/record.tsx
+++ src/resources/ts/pages/medicines/record.tsx
@@ -1,20 +1,47 @@
-import React, {useCallback, memo} from 'react';
+import React, {useState, useCallback, useRef, memo} from 'react';
 import { Link } from "react-router-dom";
 import { useQuery } from 'react-query';
 import { getMedicines } from '../../api/MedicineAPI';
+import { useNavigate } from 'react-router-dom';
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import allLocales from '@fullcalendar/core/locales-all';
 import interactionPlugin, { DateClickArg } from "@fullcalendar/interaction";
+import dayjs from 'dayjs';
 
 const MedicineRecordPage = () => {
   console.log('medicine header render');
-
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const calendarRef = useRef(null);
+  const navigate = useNavigate();
   const { data: medicines, isLoading, isError } = useQuery('medicines', getMedicines);
 
   const handleDateClick = useCallback((arg: DateClickArg) => {
-    alert(arg.dateStr);
-  }, []);
+    // 日付をクリック時
+    setSelectedDate(arg.date);
+    navigate(`/record?date=${arg.dateStr}`);
+  }, [navigate]);
+
+  const handleDatesSet = useCallback((arg) => {
+    // prev・nextをクリック時
+    // 月の初日を正しく計算
+    const currentView = arg.view;
+    const firstDayOfMonth = new Date(currentView.currentStart.getFullYear(), currentView.currentStart.getMonth(), 1);
+    setSelectedDate(firstDayOfMonth);
+    const startOfMonth = dayjs(firstDayOfMonth).startOf('month').format('YYYY-MM-DD');
+    navigate(`/record?date=${startOfMonth}`);
+  }, [navigate]);
+
+  const handleTodayClick = useCallback(() => {
+    // 「今日」をクリック時
+    const today = new Date();
+    setSelectedDate(today);
+    const todayFormat = dayjs(today).format('YYYY-MM-DD');
+    navigate(`/record?date=${todayFormat}`);
+    // カレンダーのビューを「今日」の月に切り替え
+    const calendarApi = calendarRef.current.getApi();
+    calendarApi.today();
+  }, [navigate]);
 
   return (
     <>
@@ -29,11 +56,27 @@ const MedicineRecordPage = () => {
                 </div>
                 <div style={{ backgroundColor: '#fff' }}>
                     <FullCalendar
-                        plugins={[dayGridPlugin]}
+                        ref={calendarRef}
+                        plugins={[dayGridPlugin, interactionPlugin]}
                         initialView="dayGridMonth"
                         locales={allLocales}
                         locale="ja"
                         dateClick={handleDateClick}
+                        datesSet={handleDatesSet}
+                        customButtons={{
+                            myTodayButton: {
+                              text: '今日',
+                              click: handleTodayClick,
+                            },
+                          }}
+                          headerToolbar={{
+                            left: 'prev,next myTodayButton',
+                            center: 'title',
+                            right: ''
+                          }}
+                          dayCellClassNames={(date) => {
+                            return date.date.getTime() === selectedDate.getTime() ? 'selected-date' : '';
+                        }}
                         // businessHours={{ daysOfWeek: [1, 2, 3, 4, 5] }}
                     />
                 </div>
